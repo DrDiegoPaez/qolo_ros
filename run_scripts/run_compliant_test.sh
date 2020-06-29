4157c0d5c4a6c17067ba5de31f3d0cef93b2bfc1
roboFelipe <dfpg@ieee.org>
2020-06-29 16:52:24 +0200

    - rs_camera from script

--- run_scripts/run_compliant_test.sh
+++ run_scripts/run_compliant_test.sh
@@ -15,6 +15,7 @@ while [ -d "csv_logs/test${TEST_NO}" ]; do
    TEST_NO=$(( $TEST_NO + 1 ))
 done
 LOG_FOLDER="$(pwd)/csv_logs/test${TEST_NO}"
+export LOG_FOLDER
 eval "mkdir -p ${LOG_FOLDER}/compliance"
 echo "Current Test Number : ${TEST_NO}"
 
@@ -26,27 +27,21 @@ PID_LIST+="$! "
 sleep 5
 
 #----- Launch and record realsense camera -----
-echo "Launching RealSense Camera..."
-eval "source devel/setup.bash"
-eval "roslaunch realsense2_camera rs_qolo_front_test.launch &"
-PID_LIST+="$! "
-
-# eval "rosbag record -q \
-#     -O ${LOG_FOLDER}/camera \
-#     -e '/camera_front/(.*)' \
-#     &> /dev/null &"
+# echo "Launching RealSense Camera..."
+# eval "source devel/setup.bash"
+# eval "roslaunch realsense2_camera rs_qolo_front_test.launch &"
 # PID_LIST+="$! "
 
-sleep 10
+# sleep 10
 
-eval "rostopic echo -p /camera_front/accel/sample \
-    &> ${LOG_FOLDER}/imu_accel.csv &"
-PID_LIST+="$! "
-eval "rostopic echo -p /camera_front/gyro/sample \
-    &> ${LOG_FOLDER}/imu_gyro.csv &"
-PID_LIST+="$! "
+# eval "rostopic echo -p /camera_front/accel/sample \
+#     &> ${LOG_FOLDER}/imu_accel.csv &"
+# PID_LIST+="$! "
+# eval "rostopic echo -p /camera_front/gyro/sample \
+#     &> ${LOG_FOLDER}/imu_gyro.csv &"
+# PID_LIST+="$! "
 
-sleep 5
+# sleep 5
 
 #----- Launch qolo control -----
 eval ". devel/setup.bash"
@@ -58,18 +53,18 @@ echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "Starting recording compliance test..."
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 
-eval "rostopic echo -p /qolo/compliance/raw \
-    &> ${LOG_FOLDER}/compliance/raw.csv &"
-PID_LIST+="$! "
-eval "rostopic echo -p /qolo/compliance/svr \
-    &> ${LOG_FOLDER}/compliance/svr.csv &"
-PID_LIST+="$! "
-eval "rostopic echo -p /qolo/compliance/bumper_loc \
-    &> ${LOG_FOLDER}/compliance/bumper_loc.csv &"
-PID_LIST+="$! "
-eval "rostopic echo -p /qolo/corrected_velocity \
-    &> ${LOG_FOLDER}/compliance/corr_velocity.csv &"
-PID_LIST+="$! "
+# eval "rostopic echo -p /qolo/compliance/raw \
+#     &> ${LOG_FOLDER}/compliance/raw.csv &"
+# PID_LIST+="$! "
+# eval "rostopic echo -p /qolo/compliance/svr \
+#     &> ${LOG_FOLDER}/compliance/svr.csv &"
+# PID_LIST+="$! "
+# eval "rostopic echo -p /qolo/compliance/bumper_loc \
+#     &> ${LOG_FOLDER}/compliance/bumper_loc.csv &"
+# PID_LIST+="$! "
+# eval "rostopic echo -p /qolo/corrected_velocity \
+#     &> ${LOG_FOLDER}/compliance/corr_velocity.csv &"
+# PID_LIST+="$! "
 
 # eval "rosbag record -a \
 #     -O ${LOG_FOLDER}/compliant_test \
